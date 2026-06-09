026e23cfabf09ed859679bf8bffa620f4911e249
Prabhudatta Mahapatra <prabhudattamahapatra38@gmail.com>
2026-06-09 12:07:13 +0530

    minimum-window-substring

--- sliding-windows/Longest-Substring-Without-Repeating-Characters.py
+++ sliding-windows/Longest-Substring-Without-Repeating-Characters.py
@@ -1,19 +1,11 @@
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        result=""
-        
-            
-            
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res=0
         for i in range(len(s)):
-            temp=""
+            count=set()
             for j in range(i,len(s)):
-                temp+=s[j]
-                valid=True
-                for ch in set(t):
-                    if temp.count(ch) < t.count(ch):
-                        valid=False
-                        break
-                if valid:
-                    if result=="" or len(temp)<len(result):
-                        result=temp
-        return result
+                if s[j] in count:
+                    break
+                count.add(s[j])
+            res=max(res,len(count))
+        return res
--- sliding-windows/minimum-window-substring.py
+++ sliding-windows/minimum-window-substring.py
@@ -1,15 +1,19 @@
-s="OUZODYXAZV"
-t="XYZ"
-
-n=len(t)
-result=""
-for i in range(len(s)):
-    if s[i] in t:
-        count=0
-        for j in range(i,len(s)):
-            count+=1
-            if count<=n:
-                result+=s[j]
-print(result)
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        result=""
+        
             
-
+            
+        for i in range(len(s)):
+            temp=""
+            for j in range(i,len(s)):
+                temp+=s[j]
+                valid=True
+                for ch in set(t):
+                    if temp.count(ch) < t.count(ch):
+                        valid=False
+                        break
+                if valid:
+                    if result=="" or len(temp)<len(result):
+                        result=temp
+        return result
